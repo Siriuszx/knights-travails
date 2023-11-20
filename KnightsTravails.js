f990e437a7b4a6b8f3e77447d0f7dfcb47282ea9
Siriusxz <114768817+Siriuszx@users.noreply.github.com>
2023-11-20 18:33:39 +0100

    Refactor, revise search logic

--- KnightsTravails.js
+++ KnightsTravails.js
@@ -1,70 +1,33 @@
-import Knight from "./Knight.js";
-
 class KnightsTravails {
-    #boardHeight = 7;
-    #boardWidth = 7;
-
-    board = [
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        []
-    ];
-
     #stepDirections = [[1, 2], [2, 1], [2, -1], [1, -2], [-1, -2], [-2, -1], [-2, 1], [-1, 2]];
 
     constructor() {
-        this.#buildList();
+
     }
 
-    moveKnight(coordStart, coordEnd) {
-        let queue = [new Knight(coordStart, [[coordStart[0], coordStart[1]]])];
-        let stepCount = 0;
+    moveKnight(start, end) {
+        let traversed = new Set();
+        let queue = [[start[0], start[1]]];
+        let steps = 0;
+
+        traversed.add(`[${queue[0][0]}, ${queue[0][1]}]`);
 
         while (queue.length != 0) {
-            if ((queue[0].current[0] == coordEnd[0]) && (queue[0].current[1] == coordEnd[1])) return queue[0].history;
+            if (queue[0][0] == end[0] && queue[0][1] == end[1]) return steps;
 
-            this.board[queue[0].current[0]][queue[0].current[1]].forEach(vertex => {
-                console.log(vertex);
+            for (let step of this.#stepDirections) {
+                let next = [(queue[0][0] + step[0]), (queue[0][1] + step[1])];
 
-                if ((vertex[0] != queue[0].current[0]) && (vertex[1] != queue[0].current[1])) queue.push(new Knight(vertex, queue[0].push(vertex)));
-            });
+                if (!traversed.has(`[${next[0]}, ${next[1]}]`)) {
+                    traversed.add(`[${next[0]}, ${next[1]}]`);
+                    queue.push(next);
+                }
+            }
 
             queue.shift();
-            stepCount++;
         }
 
-        return null;
-    }
-
-    #buildList() {
-        let counter = 0;
-        let trace = "";
-
-        for (let i = 0; i <= 7; i++) {
-            for (let j = 0; j <= 7; j++) {
-                this.board[i][j] = [];
-                counter++;
-                this.#stepDirections.forEach(step => {
-                    let nextStepCoord = [i + step[0], j + step[1]];
-
-                    if (this.canMove(nextStepCoord)) this.board[i][j].push(nextStepCoord);
-                });
-            }
-        }
-    }
-
-    canMove(coord) {
-        if (coord[0] > this.#boardWidth || coord[0] < 0)
-            return false;
-        if (coord[1] > this.#boardHeight || coord[1] < 0)
-            return false;
-
-        return true;
+        return steps;
     }
 }
 
--- index.js
+++ index.js
@@ -2,12 +2,5 @@ import KnightsTravails from "./KnightsTravails.js";
 
 let knight = new KnightsTravails();
 
-// let conCount = 0;
-// console.log(`Connections:`);
-// knight.board[1][2].forEach(coord => {
-//     console.log(`\t[${coord[0]}, ${coord[1]}]`);
-//     conCount++;
-// });
-// console.log(`Connections count: ${conCount}`);
-
 console.log(knight.moveKnight([3, 3], [4, 3]));
+// knight.moveKnight([3, 3], [4, 3]);
